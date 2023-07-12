a44fb15f8428e5de5416b7126f4d47b0220dc206
White Wolf <121308209+WhiteWolf91@users.noreply.github.com>
2023-07-12 16:48:45 +0300

    Alet_Dialog-Toast.kt

--- Alet_Dialog-Toast.kt
+++ Alet_Dialog-Toast.kt
@@ -1,9 +1,9 @@
 
 val alert = AlertDialog.Builder(this@MainActivity)
-        alert.setTitle("Save")
-        alert.setMessage("Kaydedildi")
+        alert.setTitle("Save") // başlık
+        alert.setMessage("Kaydedildi") // mesaj içerigi
         alert.setPositiveButton("saved") {p0, p1 ->
-            Toast.makeText(this@MainActivity,"Saved",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity,"Saved",Toast.LENGTH_SHORT).show() //Toast Gösterme Kodu (LENGHT_SHORT Gösterme Süresidir)
         }
-        alert.setIcon(R.drawable.ic_launcher_background)
-        alert.show()
+        alert.setIcon(R.drawable.ic_launcher_background) // Logo Resim Eklemek İçin Kod
+        alert.show() //ALert Dialog Göstermek İçin kod
